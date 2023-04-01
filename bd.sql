5f490d7d5d8edc4304bdc2e9827945209e1fe4da
Demba BA <badembajob@gmail.com>
2023-04-02 00:55:37 +0200

    Table vin et insertions

--- bd.sql
+++ bd.sql
@@ -4,3 +4,19 @@ CREATE TABLE users (
  login varchar(32) NOT NULL,
  mdp varchar(32) NOT NULL,
  PRIMARY KEY (`id`) );
+
+CREATE TABLE `vins` (
+    `id` int PRIMARY KEY AUTO_INCREMENT ,
+    `nom` varchar(32) NOT NULL,
+    `couleur` varchar(32) NOT NULL,
+    `libelle` varchar(64) NOT NULL,
+    `stock` int(10) NOT NULL,
+    `prix` int(10) NOT NULL
+) ;
+
+
+ALTER TABLE vins
+    ADD CONSTRAINT check_couleur CHECK (couleur IN ('rouge', 'blanc', 'rosé'));
+
+INSERT INTO `vins` (`id`, `nom`, `couleur`, `libelle`, `stock`, `prix`) VALUES (1, 'vin 1', 'rouge', 'libellé 1 ', '99', '12345');
+INSERT INTO `vins` (`id`, `nom`, `couleur`, `libelle`, `stock`, `prix`) VALUES (2, 'vin 2', 'rouge', 'libellé 2 ', '99', '12345');
